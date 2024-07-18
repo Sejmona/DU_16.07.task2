994c965abf94103913ce65fdfd3c388859248116
Simona Šmelhausová <101262454+Sejmona@users.noreply.github.com>
2024-07-18 12:35:35 +0200

    class torpedoborec

--- python.py
+++ python.py
@@ -15,6 +15,17 @@ class Fregata(Ship):
     def get_info(self):
         return f"Fregata Name: {self.name}, Length: {self.length} meters, Displacement: {self.displacement} tons, Armament: {self.armament}"
     
+class Torpedoborec(Ship):
+    def __init__(self, name, length, displacement, speed):
+        super().__init__(name, length, displacement)
+        self.speed = speed
+    
+    def get_info(self):
+        return f"Torpédoborec Name: {self.name}, Length: {self.length} meters, Displacement: {self.displacement} tons, Speed: {self.speed} knots"
+
+    
     # Test the classes
 fregata = Fregata("Frigate A", 130, 3000, "Missiles")
+torpedoborec = Torpedoborec("Destroyer B", 160, 5000, 30)
 print(fregata.get_info())
+print(torpedoborec.get_info())
